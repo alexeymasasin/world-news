09f4bd659d09048a059368743f0546eb207b7e7a
Alexey Masasin <alexeymasasin@gmail.com>
2024-07-18 18:48:22 +0500

    Search and debounce hook added

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,25 @@
+import {dateFormat} from './../../helpers/dateFormat';
+import styles from './Header.module.css';
+import HeaderButton from '../UI/Header/Button.jsx';
+import {RiNewsFill} from 'react-icons/ri';
+import {NavLink} from 'react-router-dom';
+import {RxHamburgerMenu} from 'react-icons/rx';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <NavLink to="/">
+          <h1 className={styles.title}>WORLD NEWS <RiNewsFill
+            className={styles.title_logo}/></h1>
+        </NavLink>
+        <p className={styles.date}>{dateFormat(new Date())}</p>
+      </div>
+      <div className={styles.right}>
+        <HeaderButton><RxHamburgerMenu/></HeaderButton>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
